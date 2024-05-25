b81e93dd6cd9b7a8ae2faf2023a5db4f25bd2360
n0sc3tipsum <da322@ic.ac.uk>
2024-05-25 13:53:00 +0100

    Created ros2 workspace

---
+++ build/arsenal_navigator/prefix_override/sitecustomize.py
@@ -0,0 +1,3 @@
+import sys
+sys.real_prefix = sys.prefix
+sys.prefix = sys.exec_prefix = '/home/n0sc3tipsum/ros2_ws/install/arsenal_navigator'
---
+++ src/arsenal_navigator/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+import os
+from glob import glob
+package_name = 'arsenal_navigator'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', glob('launch/*')),
+        ('share/' + package_name + '/description', glob('description/*')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='n0sc3tipsum',
+    maintainer_email='n0sc3tipsum@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'testerNode = arsenal_navigator.testerNode:main',
+        ],
+    },
+)
+
