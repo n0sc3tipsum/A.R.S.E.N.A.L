2c8420098ba3293723f071b6d3fc9077ddea7775
n0sc3tipsum <da322@ic.ac.uk>
2024-05-30 11:25:44 +0100

    Removed redundent code and delayed the joint broadcaster launch

--- src/arsenal_navigator/launch/launch_sim.launch.py
+++ src/arsenal_navigator/launch/launch_sim.launch.py
@@ -6,8 +6,8 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.actions import RegisterEventHandler
-from launch.event_handlers import OnProcessExit
+from launch.actions import RegisterEventHandler, TimerAction
+from launch.event_handlers import OnProcessExit, OnProcessStart
 from launch_ros.actions import Node
 from launch.substitutions import LaunchConfiguration, Command
 from launch.actions import DeclareLaunchArgument
@@ -16,35 +16,22 @@ from launch.actions import DeclareLaunchArgument
 
 def generate_launch_description():
 
-
-    # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
-    # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
-
-    package_name='arsenal_navigator' #<--- CHANGE ME
+    package_name='arsenal_navigator' 
     pkg_path = os.path.join(get_package_share_directory('arsenal_navigator'))
+    
+    twist_mux_params = os.path.join(get_package_share_directory(package_name),'config','twist_mux.yaml')
+    rviz_config_path = os.path.join(pkg_path, 'config/sim_bot.rviz')
+    gazebo_params_file = os.path.join(get_package_share_directory(package_name),'config','gazebo_params.yaml')
+    robot_description = Command(['ros2 param get --hide-type /robot_state_publisher robot_description'])
+    controller_params_file = os.path.join(get_package_share_directory(package_name),'config','controllers.yaml')
 
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory(package_name),'launch','rsp.launch.py'
-                )]), launch_arguments={'use_sim_time': 'true', 'use_ros2_control': 'true'}.items()
+                )]), launch_arguments={'use_sim_time': 'true', 'use_ros2_control': 'true', 'sim_lidar':'true'}.items()
     )
 
-    '''joystick = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory(package_name),'launch','joystick.launch.py'
-                )]), launch_arguments={'use_sim_time': 'true'}.items()
-    )'''
 
-    rviz_config_path = os.path.join(pkg_path, 'config/sim_bot.rviz')
-    rviz_node = Node(
-        package='rviz2',
-        executable='rviz2',
-        name='rviz2',
-        output='screen',
-        arguments=['d', LaunchConfiguration('rvizconfig')],
-    )
-
-    twist_mux_params = os.path.join(get_package_share_directory(package_name),'config','twist_mux.yaml')
     twist_mux = Node(
             package="twist_mux",
             executable="twist_mux",
@@ -52,7 +39,6 @@ def generate_launch_description():
             remappings=[('/cmd_vel_out','/diff_cont/cmd_vel_unstamped')]
         )
 
-    gazebo_params_file = os.path.join(get_package_share_directory(package_name),'config','gazebo_params.yaml')
 
     # Include the Gazebo launch file, provided by the gazebo_ros package
     gazebo = IncludeLaunchDescription(
@@ -68,10 +54,26 @@ def generate_launch_description():
                         output='screen')
 
 
+    controller_manager = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        parameters=[{'robot_description': robot_description},
+                    controller_params_file]
+    )
+
+    delayed_controller_manager = TimerAction(period=3.0, actions=[controller_manager])
+
     diff_drive_spawner = Node(
         package="controller_manager",
         executable="spawner",
         arguments=["diff_cont"],
+    )
+
+    delayed_diff_drive_spawner = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=spawn_entity,
+            on_exit=[diff_drive_spawner],
+        )
     )
 
     joint_broad_spawner = Node(
@@ -80,38 +82,22 @@ def generate_launch_description():
         arguments=["joint_broad"],
     )
 
-
-
-    # Code for delaying a node (I haven't tested how effective it is)
-    # 
-    # First add the below lines to imports
-    # from launch.actions import RegisterEventHandler
-    # from launch.event_handlers import OnProcessExit
-    #
-    # Then add the following below the current diff_drive_spawner
-    delayed_diff_drive_spawner = RegisterEventHandler(
-         event_handler=OnProcessExit(
-             target_action=spawn_entity,
-             on_exit=[diff_drive_spawner],
-         )
+    delayed_joint_broad_spawner = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=spawn_entity,
+            on_exit=[joint_broad_spawner],
+        )
     )
-    #
-    # Replace the diff_drive_spawner in the final return with delayed_diff_drive_spawner
 
 
-
-    # Launch them all!
+    # Launch them all
     return LaunchDescription([
-        DeclareLaunchArgument(
-            name='rvizconfig', default_value=rviz_config_path,
-            description='path to rviz vonfig file'
-        ),
         rsp,
         #joystick,
         twist_mux,
         gazebo,
         spawn_entity,
+        #controller_manager,
         delayed_diff_drive_spawner,
-        joint_broad_spawner,
-        rviz_node
+        delayed_joint_broad_spawner
     ])
