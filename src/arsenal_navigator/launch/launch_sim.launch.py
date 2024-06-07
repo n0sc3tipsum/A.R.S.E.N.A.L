153eb5f99051689f0f7c5a92fe7de86a1b70ff72
n0sc3tipsum <da322@ic.ac.uk>
2024-06-07 02:06:07 +0100

    removed sim_lidar parameter

--- src/arsenal_navigator/launch/launch_sim.launch.py
+++ src/arsenal_navigator/launch/launch_sim.launch.py
@@ -28,7 +28,7 @@ def generate_launch_description():
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory(package_name),'launch','rsp.launch.py'
-                )]), launch_arguments={'use_sim_time': 'true', 'use_ros2_control': 'true', 'sim_lidar':'true'}.items()
+                )]), launch_arguments={'use_sim_time': 'true', 'use_ros2_control': 'true'}.items()
     )
 
 
