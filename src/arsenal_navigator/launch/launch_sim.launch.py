3a523249a2f71d5579ddf6999959c39665a5d424
n0sc3tipsum <da322@ic.ac.uk>
2024-06-10 19:41:26 +0100

    updated simulation launch file

--- src/arsenal_navigator/launch/launch_sim.launch.py
+++ src/arsenal_navigator/launch/launch_sim.launch.py
@@ -97,7 +97,9 @@ def generate_launch_description():
         twist_mux,
         gazebo,
         spawn_entity,
-        #controller_manager,
-        delayed_diff_drive_spawner,
-        delayed_joint_broad_spawner
+        #delayed_controller_manager,
+        #delayed_diff_drive_spawner,
+        #delayed_joint_broad_spawner,
+        diff_drive_spawner,
+        joint_broad_spawner
     ])
