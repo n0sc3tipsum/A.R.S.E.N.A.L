f5e4ca3cf1ae505b3444280c9fae476dd2ef6cfd
n0sc3tipsum <da322@ic.ac.uk>
2024-06-10 20:38:09 +0100

    added setup and CMake files for package config

---
+++ arsenal_bot/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+from glob import glob
+
+package_name = 'arsenal_bot'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', glob('launch/*')),
+        ('share/' + package_name + '/description', glob('description/*')),
+        #('share/' + package_name + '/rviz', glob('rviz/*')),
+        ('share/' + package_name + '/config', glob('config/*')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='n0sc3tipsum',
+    maintainer_email='n0sc3tipsum@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
