6da715c286931e240bb16da888821935b3697c77
n0sc3tipsum <131246875+n0sc3tipsum@users.noreply.github.com>
2024-06-19 02:20:02 +0400

    Update Battery.h

--- include/Battery.h
+++ include/Battery.h
@@ -7,9 +7,9 @@
 #define VOLTAGE_TO_LEVEL    3.63e-3
 #define VOLTAGE_TO_CURRENT  2.325
 
-#define MOTOR_PIN           25      //A2
-#define LOGIC_PIN           26      //A1
-#define BATTERY_PIN         27      //A0
+#define MOTOR_PIN           35     //A5
+#define LOGIC_PIN           32      //A4
+#define BATTERY_PIN         33      //A3
 
 
 class Battery
