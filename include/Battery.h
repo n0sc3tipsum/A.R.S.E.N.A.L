cfb250cfbb6b5577a94e8a93d45d71c81d62320b
n0sc3tipsum <131246875+n0sc3tipsum@users.noreply.github.com>
2024-06-19 02:24:34 +0400

    Update Battery.h

--- include/Battery.h
+++ include/Battery.h
@@ -3,9 +3,6 @@
 
 #include <Arduino.h>
 
-#define VALUE_TO_VOLTAGE    8.06e-4
-#define VOLTAGE_TO_LEVEL    3.63e-3
-#define VOLTAGE_TO_CURRENT  2.325
 
 #define MOTOR_PIN           35     //A5
 #define LOGIC_PIN           32      //A4
