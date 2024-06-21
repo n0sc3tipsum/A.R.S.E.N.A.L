73b61cc03e2f51c9de3268c05e908f384e3bf67c
Ziean Ahmed Sheikh <137586108+zieanash@users.noreply.github.com>
2024-06-21 16:29:52 +0100

    Add files via upload

---
+++ ConnectionManager.vb
@@ -0,0 +1,244 @@
+﻿Imports System.Net.Http
+Imports System.Net.Http.Headers
+Imports System.IO
+Imports System.Text
+
+Partial Public Class Form1
+
+    Public Enum movementDirection
+        Forward
+        Backward
+        Left
+        Right
+
+        ForwardAndLeft
+        ForwardAndRight
+        BackwardAndLeft
+        BackwardAndRight
+
+        Halt
+    End Enum
+
+    Public Enum launchCategory
+        Server
+        Sim
+        Navigating
+        Mapping
+        LiDAR
+        PiCam
+    End Enum
+
+    Dim client
+    Public Function connectToServer(ip As String) As Boolean
+        client = New HttpClient()
+        Try
+            client.BaseAddress = New Uri("http://" & ip)
+            client.Timeout = TimeSpan.FromSeconds(45)
+
+            Return True
+        Catch
+            client.dispose()
+            Return False
+        End Try
+    End Function
+
+    Private Async Function pollingLoop() As Task
+        Try
+            While (Await timer.WaitForNextTickAsync(cts.Token))
+                If connected Then
+                    Dim readSensorDataTask = Task.WhenAll(getSensorDataAsync(infoType.powerConsumption), getSensorDataAsync(infoType.remainingCharge), getSensorDataAsync(infoType.batteryVoltage))
+                    Await readSensorDataTask
+                End If
+            End While
+        Catch ex As OperationCanceledException
+            rtbLogs.AppendText("Operation cancelled exception - No longer connected")
+        End Try
+    End Function
+
+    Dim polledData_powerConsumption, polledData_remainingCharge, polledData_batteryVoltage
+    Private Sub storeSensorData(data As String, sensor As infoType)
+        If (sensor = infoType.powerConsumption) Then
+            polledData_powerConsumption = data
+        End If
+
+        If (sensor = infoType.remainingCharge) Then
+            polledData_remainingCharge = data
+        End If
+
+        If (sensor = infoType.batteryVoltage) Then
+            polledData_batteryVoltage = data
+        End If
+    End Sub
+
+    Public Async Function getSensorDataAsync(sensor As infoType) As Task(Of Boolean)
+        Dim sensorEndpoint As String
+        If (sensor = infoType.powerConsumption) Then sensorEndpoint = "/readPowerConsumption"
+        If (sensor = infoType.remainingCharge) Then sensorEndpoint = "/readRemainingCharge"
+        If (sensor = infoType.batteryVoltage) Then sensorEndpoint = "/readBatteryVoltage"
+
+        Dim result As String = Await resourceRequestAsync(sensorEndpoint)
+
+        updateInfoLabels(result, sensor)
+
+        If result <> "ERROR" Then
+            storeSensorData(result, sensor)
+            Return True
+        End If
+
+        Return False
+    End Function
+
+    Dim currentMovementDirection As movementDirection = movementDirection.Halt
+    Public Async Function sendMoveInstructionAsync(direction As movementDirection) As Task(Of Boolean)
+        If currentMovementDirection = direction Then Return True
+
+        currentMovementDirection = direction
+
+        Dim directionEndpoint As String
+        If (direction = movementDirection.Forward) Then directionEndpoint = "/moveForward"
+        If (direction = movementDirection.Backward) Then directionEndpoint = "/moveBackward"
+        If (direction = movementDirection.Left) Then directionEndpoint = "/turnLeft"
+        If (direction = movementDirection.Right) Then directionEndpoint = "/turnRight"
+
+        If (direction = movementDirection.ForwardAndLeft) Then directionEndpoint = "/moveForwardAndLeft"
+        If (direction = movementDirection.ForwardAndRight) Then directionEndpoint = "/moveForwardAndRight"
+        If (direction = movementDirection.BackwardAndLeft) Then directionEndpoint = "/moveBackwardAndLeft"
+        If (direction = movementDirection.BackwardAndRight) Then directionEndpoint = "/moveBackwardAndRight"
+
+        If (direction = movementDirection.Halt) Then directionEndpoint = "/haltMovement"
+
+        Dim result As String = Await resourceRequestAsync(directionEndpoint)
+
+        rtbLogs.AppendText(result + vbCrLf)
+
+        If (result <> "ERROR") Then
+            Return True
+        End If
+
+        Return False
+
+    End Function
+
+    Public Async Function sendLaunchRequest(category As launchCategory) As Task(Of Boolean)
+        Dim categoryEndpoint, result As String
+        If (category = launchCategory.Server) Then categoryEndpoint = "/launchServer"
+        If (category = launchCategory.Sim) Then categoryEndpoint = "/launchSim"
+        If (category = launchCategory.Mapping) Then categoryEndpoint = "/launchMapping"
+        If (category = launchCategory.Navigating) Then
+            categoryEndpoint = "/launchNavigating"
+
+            Dim fileDialog = New OpenFileDialog()
+            fileDialog.Filter = "YAML|*.yaml;*.yml"
+            fileDialog.RestoreDirectory = True
+
+            If (fileDialog.ShowDialog = DialogResult.OK) Then
+                result = Await resourceProvideAsync(categoryEndpoint, fileDialog.FileName)
+            Else
+                Return False
+            End If
+
+        Else
+            result = Await resourceRequestAsync(categoryEndpoint)
+        End If
+
+        rtbLogs.AppendText(result + vbCrLf)
+
+        If (result <> "ERROR") Then
+            Return True
+        End If
+
+        Return False
+    End Function
+
+    Public Async Function sendSensorStartRequest(category As launchCategory) As Task(Of Boolean)
+        Dim categoryEndpoint As String
+        If (category = launchCategory.PiCam) Then categoryEndpoint = "/startPiCam"
+        If (category = launchCategory.LiDAR) Then categoryEndpoint = "/startLiDAR"
+
+        Try
+            Dim content As New StringContent(tbRobotIP.Text, Encoding.UTF8, "application/json")
+            Dim response As HttpResponseMessage = Await client.PostAsync(categoryEndpoint, content)
+            response.EnsureSuccessStatusCode()
+            Dim responseContent As String = Await response.Content.ReadAsStringAsync()
+            rtbLogs.AppendText(responseContent)
+
+            Return True
+        Catch e As Exception
+            Return False
+        End Try
+    End Function
+
+    Public Async Function sendToggleAutoAsync() As Task(Of Boolean)
+        Dim result As String = Await resourceRequestAsync("/toggleAutonomousNavigation")
+
+        rtbLogs.AppendText(result + vbCrLf)
+
+        If (result <> "ERROR") Then
+            Return True
+        End If
+        Return False
+
+    End Function
+
+    Public Async Function startFoxgloveBridge() As Task(Of Boolean)
+        Dim result As String = Await resourceRequestAsync("/startFoxgloveBridge")
+
+        rtbLogs.AppendText(result + vbCrLf)
+
+        If (result <> "ERROR") Then Return True
+
+        Return False
+    End Function
+
+
+    Private Async Function resourceRequestAsync(endpoint As String) As Task(Of String)
+        If Not connected Then
+            Return "ERROR"
+        End If
+
+        Try
+            Dim responseTask As Task(Of HttpResponseMessage) = client.GetAsync(endpoint)
+            Dim response = Await responseTask
+
+            If response.IsSuccessStatusCode Then
+                Dim responseContent = response.Content.ReadAsStringAsync().Result
+                Return responseContent
+            Else
+                Return "ERROR"
+            End If
+        Catch e As Exception
+            rtbLogs.AppendText(e.ToString() + vbCrLf)
+            Return "ERROR"
+        End Try
+    End Function
+
+    Private Async Function resourceProvideAsync(endpoint As String, filePath As String) As Task(Of String)
+        If Not connected Then
+            Return "ERROR"
+        End If
+
+        Try
+            Dim fileBytes As Byte() = File.ReadAllBytes(filePath)
+
+            Dim content As New ByteArrayContent(fileBytes)
+            content.Headers.ContentType = New System.Net.Http.Headers.MediaTypeHeaderValue("application/octet-stream")
+
+            Dim responseTask As Task(Of HttpResponseMessage) = client.PutAsync(endpoint, content)
+            Dim response = Await responseTask
+
+            If response.IsSuccessStatusCode Then
+                Dim responseContent = response.Content.ReadAsStringAsync().Result
+                Return responseContent
+            Else
+                rtbLogs.AppendText(response.Content.ReadAsStringAsync().Result + vbCrLf)
+                Return "ERROR"
+            End If
+        Catch e As Exception
+            rtbLogs.AppendText(e.ToString() + vbCrLf)
+            Return "ERROR"
+        End Try
+    End Function
+
+End Class
+
+
---
+++ Form1.vb
@@ -0,0 +1,195 @@
+﻿Imports System.Threading
+Partial Public Class Form1
+
+    Dim connected As Boolean = False
+
+    Public Enum infoType As Integer
+        powerConsumption
+        remainingCharge
+        batteryVoltage
+    End Enum
+
+    Public Sub updateInfoLabels(data As String, infoType As infoType)
+        If infoType = infoType.powerConsumption Then lblPowerConsumption.Text = data
+        If infoType = infoType.remainingCharge Then lblRemainingCharge.Text = data
+        If infoType = infoType.batteryVoltage Then lblBatteryVoltage.Text = data
+    End Sub
+
+    Public Sub clearInfoLabels()
+        lblPowerConsumption.Text = "-----"
+        lblRemainingCharge.Text = "-----"
+        lblBatteryVoltage.Text = "-----"
+    End Sub
+
+    Dim timer As PeriodicTimer
+    Dim timertask As Task
+    Dim cts As CancellationTokenSource
+    Private Async Sub btnConnect_Click(sender As Object, e As EventArgs) Handles btnConnect.Click
+        If connected = False Then
+            If (connectToServer(tbIP.Text)) Then
+                connected = True
+
+                tbIP.Enabled = False
+                lblConnectionStatus.Text = "Connected"
+                btnConnect.Text = "Disconnect"
+
+                timer = New PeriodicTimer(TimeSpan.FromMilliseconds(3000))
+                cts = New CancellationTokenSource
+                timertask = pollingLoop()
+
+                Try
+                    Await timertask
+                Catch ex As OperationCanceledException
+                    ' no longer connected
+                End Try
+
+            Else
+            End If
+        Else
+            connected = False
+            polledData_powerConsumption = ""
+            polledData_remainingCharge = ""
+            polledData_batteryVoltage = ""
+
+            cts.Cancel()
+            Try
+                Await timertask
+            Catch ex As OperationCanceledException
+                ' operation canceled
+            End Try
+            cts.Dispose()
+
+            client.dispose()
+
+            tbIP.Enabled = True
+            lblConnectionStatus.Text = "Not connected"
+            btnConnect.Text = "Connect"
+            clearInfoLabels()
+        End If
+    End Sub
+
+
+
+
+
+
+    Private Async Sub btnToggleAutoMove_Click(sender As Object, e As EventArgs) Handles btnToggleAutoMove.Click
+        Await sendToggleAutoAsync()
+    End Sub
+
+
+    Private Async Sub btnLeft_MouseDown(sender As Object, e As EventArgs) Handles btnLeft.MouseDown
+        Await sendMoveInstructionAsync(movementDirection.Left)
+    End Sub
+    Private Async Sub btnRight_MouseDown(sender As Object, e As EventArgs) Handles btnRight.MouseDown
+        Await sendMoveInstructionAsync(movementDirection.Right)
+    End Sub
+    Private Async Sub btnForward_MouseDown(sender As Object, e As EventArgs) Handles btnForward.MouseDown
+        Await sendMoveInstructionAsync(movementDirection.Forward)
+    End Sub
+    Private Async Sub btnBackward_MouseDown(sender As Object, e As EventArgs) Handles btnBackward.MouseDown
+        Await sendMoveInstructionAsync(movementDirection.Backward)
+    End Sub
+
+    Private Async Sub btnForwardAndLeft_MouseDown(sender As Object, e As EventArgs) Handles btnForwardAndLeft.MouseDown
+        Await sendMoveInstructionAsync(movementDirection.ForwardAndLeft)
+    End Sub
+    Private Async Sub btnForwardAndRight_MouseDown(sender As Object, e As EventArgs) Handles btnForwardAndRight.MouseDown
+        Await sendMoveInstructionAsync(movementDirection.ForwardAndRight)
+    End Sub
+    Private Async Sub btnBackwardAndLeft_MouseDown(sender As Object, e As EventArgs) Handles btnBackwardAndLeft.MouseDown
+        Await sendMoveInstructionAsync(movementDirection.BackwardAndLeft)
+    End Sub
+    Private Async Sub btnBackwardAndRight_MouseDown(sender As Object, e As EventArgs) Handles btnBackwardAndRight.MouseDown
+        Await sendMoveInstructionAsync(movementDirection.BackwardAndRight)
+    End Sub
+
+    Private Async Sub btnMovement_MouseUp(sender As Object, e As MouseEventArgs) Handles btnForward.MouseUp, btnLeft.MouseUp, btnRight.MouseUp, btnBackward.MouseUp
+        Await sendMoveInstructionAsync(movementDirection.Halt)
+    End Sub
+
+
+    Private Async Sub Form1_KeyUp(sender As Object, e As KeyEventArgs) Handles MyBase.KeyUp
+        If e.KeyCode() = Keys.W Or e.KeyCode() = Keys.A Or e.KeyCode() = Keys.S Or e.KeyCode() = Keys.D Then Then
+            Await sendMoveInstructionAsync(movementDirection.Halt)
+        End If
+        If e.KeyCode() = Keys.Q Or e.KeyCode() = Keys.E Or e.KeyCode() = Keys.Z Or e.KeyCode() = Keys.C Then Then
+            Await sendMoveInstructionAsync(movementDirection.Halt)
+        End If
+    End Sub
+    Private Async Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles MyBase.KeyDown
+        If e.KeyCode() = Keys.W Then
+            Await sendMoveInstructionAsync(movementDirection.Forward)
+        End If
+
+        If e.KeyCode() = Keys.A Then
+            Await sendMoveInstructionAsync(movementDirection.Left)
+        End If
+
+        If e.KeyCode() = Keys.S Then
+            Await sendMoveInstructionAsync(movementDirection.Backward)
+        End If
+
+        If e.KeyCode() = Keys.D Then
+            Await sendMoveInstructionAsync(movementDirection.Right)
+        End If
+
+
+
+        If e.KeyCode() = Keys.Q Then
+            Await sendMoveInstructionAsync(movementDirection.ForwardAndLeft)
+        End If
+
+        If e.KeyCode() = Keys.E Then
+            Await sendMoveInstructionAsync(movementDirection.ForwardAndRight)
+        End If
+
+        If e.KeyCode() = Keys.Z Then
+            Await sendMoveInstructionAsync(movementDirection.BackwardAndLeft)
+        End If
+
+        If e.KeyCode() = Keys.C Then
+            Await sendMoveInstructionAsync(movementDirection.BackwardAndRight)
+        End If
+    End Sub
+
+    Const launchLabelClickText = "Attempting request send"
+    Private Sub updateLaunchStatusLabel(lbl As Label, launchSuccessful As Boolean)
+        If launchSuccessful Then
+            lbl.Text = "Sent request successfully"
+        Else
+            lbl.Text = "Failed to send request"
+        End If
+    End Sub
+
+    Private Async Sub btnStartNavigating_Click(sender As Object, e As EventArgs) Handles btnStartNavigating.Click
+        lblStartNavigating.Text = launchLabelClickText
+        updateLaunchStatusLabel(lblStartNavigating, Await sendLaunchRequest(launchCategory.Navigating))
+    End Sub
+    Private Async Sub btnStartServer_server_Click(sender As Object, e As EventArgs) Handles btnStartServer.Click
+        lblStartServer.Text = launchLabelClickText
+        updateLaunchStatusLabel(lblStartServer, Await sendLaunchRequest(launchCategory.Server))
+    End Sub
+    Private Async Sub btnStartSim_Click(sender As Object, e As EventArgs) Handles btnStartSim.Click
+        lblStartSim.Text = launchLabelClickText
+        updateLaunchStatusLabel(lblStartSim, Await sendLaunchRequest(launchCategory.Sim))
+    End Sub
+    Private Async Sub btnStartMapping_Click(sender As Object, e As EventArgs) Handles btnStartMapping.Click
+        lblStartMapping.Text = launchLabelClickText
+        updateLaunchStatusLabel(lblStartMapping, Await sendLaunchRequest(launchCategory.Mapping))
+    End Sub
+    Private Async Sub btnStartLiDAR_Click(sender As Object, e As EventArgs) Handles btnStartLiDAR.Click
+        lblStartLiDAR.Text = launchLabelClickText
+        updateLaunchStatusLabel(lblStartLiDAR, Await sendLaunchRequest(launchCategory.LiDAR))
+    End Sub
+
+    Private Async Sub btnStartPiCam_Click(sender As Object, e As EventArgs) Handles btnStartPiCam.Click
+        lblStartPiCam.Text = launchLabelClickText
+        updateLaunchStatusLabel(lblStartPiCam, Await sendSensorStartRequest(launchCategory.PiCam))
+    End Sub
+    Private Async Sub btnStartFoxgloveBridge_Click(sender As Object, e As EventArgs) Handles btnStartFoxgloveBridge.Click
+        lblStartFoxgloveBridge.Text = launchLabelClickText
+        updateLaunchStatusLabel(lblStartFoxgloveBridge, Await startFoxgloveBridge())
+    End Sub
+
+End Class
